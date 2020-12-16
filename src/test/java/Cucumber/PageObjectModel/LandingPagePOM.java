1312f606a2463637de398dd6ae6137875b212087
istvangercsak <istvan.gercsak@gmail.com>
2020-12-16 08:46:48 +0000

    Expand landing page POM

--- src/test/java/Cucumber/PageObjectModel/LandingPagePOM.java
+++ src/test/java/Cucumber/PageObjectModel/LandingPagePOM.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LandingPagePOM{
+public class LandingPagePOM {
 
     public String landingPageUrl = "https://budgetcalculator-thirdproject.herokuapp.com/";
     String userNameID = "username";
@@ -13,6 +13,10 @@ public class LandingPagePOM{
     String signUpButtonCSS = "body > div.container-fluid > div > div > div > a > button";
     String githubIconCSS = "body > footer > div > div > div.col-sm-12.col-md-6.list-inline.social-link-container > ul > li:nth-child(1) > a";
     String linkedInIconCSS = "body > footer > div > div > div.col-sm-12.col-md-6.list-inline.social-link-container > ul > li:nth-child(2) > a";
+
+    String githubIconHref = "https://github.com/IstvanGercsak";
+    String linkedinIconHref = "https://www.linkedin.com/in/istvangercsak/";
+
 
     public WebElement userNameID(WebDriver driver) {
         return driver.findElement(By.id(userNameID));
